ee5641c5c9aab8c621073f9baaa24528fef706be
JiriVanek <vanek2@rti.zcu.cz>
2017-05-19 12:35:27 +0200

    avg 73.71 max 79.12 min 68.44 from 120

--- src/cz/zcu/kiv/eeg/gtn/application/classification/MixDeepLearning4j.java
+++ src/cz/zcu/kiv/eeg/gtn/application/classification/MixDeepLearning4j.java
@@ -123,7 +123,7 @@ public class MixDeepLearning4j implements IERPClassifier {
                         .nOut(64)
                         .weightInit(WeightInit.RELU)
                         .activation(Activation.RELU)
-                        .corruptionLevel(0.) // Set level of corruption
+                        .corruptionLevel(0.1) // Set level of corruption
                         .lossFunction(LossFunctions.LossFunction.XENT)
                         .build())
                 .layer(1, new DenseLayer.Builder().nIn(64).nOut(128)
